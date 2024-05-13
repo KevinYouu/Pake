fd04526531fd7baafaa1282fa4ea3943de0471ac
KevinYouu <zdy295@gmail.com>
2024-05-13 22:51:09 +0800

    refactor: 关闭逻辑

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,9 +6,8 @@
 mod app;
 mod util;
 
-use app::{invoke, menu, window};
+use app::{invoke, window};
 use invoke::{download_file, download_file_by_binary};
-use menu::{get_system_tray, system_tray_handle};
 use tauri::{GlobalShortcutManager, Manager};
 use tauri_plugin_window_state::Builder as windowStatePlugin;
 use util::{get_data_dir, get_pake_config};
@@ -19,15 +18,6 @@ pub fn run_app() {
     let data_dir = get_data_dir(tauri_config);
 
     let mut tauri_app = tauri::Builder::default();
-
-    let show_system_tray = pake_config.show_system_tray();
-    let system_tray = get_system_tray();
-
-    if show_system_tray {
-        tauri_app = tauri_app
-            .system_tray(system_tray)
-            .on_system_tray_event(system_tray_handle);
-    }
 
     // Save the value of toggle_app_shortcut before pake_config is moved
     let activation_shortcut = pake_config.windows[0].activation_shortcut.clone();
@@ -65,13 +55,6 @@ pub fn run_app() {
         })
         .on_window_event(|event| {
             if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
-                #[cfg(target_os = "macos")]
-                {
-                    event.window().minimize().unwrap();
-                    event.window().hide().unwrap();
-                }
-
-                #[cfg(not(target_os = "macos"))]
                 event.window().close().unwrap();
 
                 api.prevent_close();
